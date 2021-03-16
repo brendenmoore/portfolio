979b5ebf4dc71df445f91ee7a1019489c267f4e1
Brenden <52762708+brendenmoore@users.noreply.github.com>
2021-03-16 11:19:41 -0400

    update to use gatsby-plugin-image

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,21 +1,22 @@
 import React from "react";
-import profile from "../images/profile.jpg";
+import { StaticImage } from "gatsby-plugin-image";
 
 const Hero = () => {
   return (
     <main className="container py-12 md:py-16 grid grid-cols-3 gap-6 sm:grid-cols-2 h-96 sm:h-80vh">
       <div className="flex justify-center items-center w-full col-start-1 sm:col-start-2">
-        <img
-          src={profile}
+        <StaticImage
+          src="../images/profile.jpg"
           alt="Profile"
-          className="object-cover w-28 h-28 rounded-full sm:w-full sm:h-full sm:rounded-md max-h-80 max-w-lg"
-        ></img>
+          placeholder="blurred"
+          className="object-cover w-28 h-28 rounded-full sm:w-full sm:h-full sm:rounded-md max-h-80 max-w-lg shadow"
+        ></StaticImage>
       </div>
-      <div className="space-y-3 col-span-2 col-start-2 row-start-1 sm:col-span-1 sm:col-start-1 flex flex-col justify-center">
+      <div className="space-y-3 col-span-2 col-start-2 row-start-1 sm:col-span-1 sm:col-start-1 flex flex-col justify-center text-center sm:text-left">
         <h1 className="text-5xl sm:text-6xl md:text-7xl font-serif">
-          Brenden Moore
+          Brenden <span className="text-6xl sm:text-7xl">Moore</span>
         </h1>
-        <h2 className="text-3xl text-blue-500">Web Developer</h2>
+        <h2 className="text-2xl sm:text-3xl text-blue-500">Web Developer</h2>
       </div>
     </main>
   );
