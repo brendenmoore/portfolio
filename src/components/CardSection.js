f553dd8e7e7b692f85046cceca24fe39b99a5cda
Brenden <52762708+brendenmoore@users.noreply.github.com>
2021-03-16 14:26:08 -0400

    styles

--- src/components/CardSection.js
+++ src/components/CardSection.js
@@ -3,12 +3,12 @@ import { Link } from "gatsby";
 
 const CardSection = () => {
   return (
-    <section className="bg-blue-500 mt-8">
+    <section className="bg-blue-400 mt-8">
       <div className="container flex justify-center">
         <div className="grid sm:grid-cols-3 gap-4 relative -top-8 text-2xl md:text-4xl font-thin w-4/6 sm:w-full">
           <Link
             to="/resume"
-            className="bg-white rounded shadow border-2 border-black h-16 sm:h-32 flex items-center justify-center -ml-5 mr-5 sm:m-0"
+            className="bg-white rounded shadow border-2 border-black h-16 sm:h-32 flex items-center justify-center"
           >
             <h1>Resume</h1>
           </Link>
@@ -20,7 +20,7 @@ const CardSection = () => {
           </Link>
           <Link
             to="/resume"
-            className="bg-white rounded shadow border-2 border-black h-16 sm:h-32 flex items-center justify-center -mr-5 ml-5 sm:m-0"
+            className="bg-white rounded shadow border-2 border-black h-16 sm:h-32 flex items-center justify-center"
           >
             <h1>GitHub</h1>
           </Link>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Navbar from '../components/Navbar'
-import Hero from '../components/Hero'
-import CardSection from '../components/CardSection'
+import Navbar from "../components/Navbar";
+import Hero from "../components/Hero";
+import CardSection from "../components/CardSection";
 
 const index = () => {
   return (
